d1770e1021dfba67312a389ad1148ce87134c44a
Alberto Amo Marin <albertoamo60@gmail.com>
2021-01-18 13:51:43 +0100

    [ADD] - routes for components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SpainComponent } from './spain/spain.component';
 import { EeuuComponent } from './eeuu/eeuu.component';
 import { FranceComponent } from './france/france.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SpainComponent,
     EeuuComponent,
-    FranceComponent
+    FranceComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
